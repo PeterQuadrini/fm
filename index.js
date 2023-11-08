92946ce15a83746a2d3e9dec2395a3e434881391
pietroquadrinidev <130556191+pietroquadrinidev@users.noreply.github.com>
2023-11-08 16:20:43 +0100

    ok

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ init();
 container();
 
 function init() {
-    var list = document.getElementById("list");
+    document.body.style.overflowY = 'hidden'
+    var modal = document.getElementById("modalC");
+    var modalH1 = document.getElementById("modal-h1");
+    var modalP = document.getElementById("modal-p");
+    var vh = 100/h
+    modalH1.style.fontSize = 25*vh + 'vh'
+    modalP.style.fontSize = 15*vh + 'vh'
+    var modalContainer = document.getElementById("modal-container");
+    modal.style.width = w + 'px'
+    modal.style.height = h + 'px'
+    modalContainer.style.height = h/3 + 'px'
+    modalContainer.style.maxWidth = w * 3/4 + 'px'
+    modalContainer.style.minWidth = w * 2/5 + 'px'
+    console.log('modal', modal)
+   /*  var list = document.getElementById("list");
     var dropdown = document.getElementById("dropdown");
     if(window.innerWidth < 600) {
         list.style.display = 'none'
@@ -12,16 +26,28 @@ function init() {
     } else {
         dropdown.style.display = 'none'
         list.style.display = 'flex'
-    }
+    } */
+}
+
+function buttonClick() {
+    console.log('ciao')
+    document.getElementById("modalC").style.display = 'none';
+    document.getElementById("html-body").style.display = 'inline-block';
+    var video = document.getElementById("video");
+    video.addEventListener('click', function () {
+        console.log('ciiii')
+        document.body.style.overflowY = 'auto'
+        video.play()
+    })
+    video.click()
+    console.log('video', video)
 }
 
 function container() {
     var container = document.getElementById("container");
-    var box = document.getElementById("box");
     var works = document.getElementById("works");
     var workItem = document.getElementsByClassName('work-item')
-    container.style.height = window.innerHeight+'px';
-    box.style.height = window.innerHeight+'px';
+    container.style.width = w+'px';
     works.style.width = window.innerWidth+'px';
     console.log('workitem', workItem)
     const divider = window.innerWidth > 1032
